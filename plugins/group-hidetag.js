715ef982aaf6d09509683bca923a62622ef4594c
alexbotsx <toscren0@gmail.com>
2025-07-12 21:38:05 -0500

    dxd

--- plugins/group-hidetag.js
+++ plugins/group-hidetag.js
@@ -1,9 +1,5 @@
 import {generateWAMessageFromContent} from '@whiskeysockets/baileys';
 import * as fs from 'fs';
-// Para configurar o idioma, na raiz do projeto altere o arquivo config.json
-// Para configurar el idioma, en la raíz del proyecto, modifique el archivo config.json.
-// To set the language, in the root of the project, modify the config.json file.
-
 const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
   try {
     const users = participants.map((u) => conn.decodeJid(u.id));
@@ -12,9 +8,6 @@ const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
     const msg = conn.cMod(m.chat, generateWAMessageFromContent(m.chat, {[m.quoted ? q.mtype : 'extendedTextMessage']: m.quoted ? c.message[q.mtype] : {text: '' || c}}, {quoted: m, userJid: conn.user.id}), text || q.text, conn.user.jid, {mentions: users});
     await conn.relayMessage(m.chat, msg.message, {messageId: msg.key.id});
   } catch {
-    /**
-[ By @NeKosmic || https://github.com/NeKosmic/ ]
-**/
 
     const users = participants.map((u) => conn.decodeJid(u.id));
     const quoted = m.quoted ? m.quoted : m;
@@ -22,7 +15,7 @@ const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
     const isMedia = /image|video|sticker|audio/.test(mime);
     const more = String.fromCharCode(8206);
     const masss = more.repeat(850);
-    const htextos = `${text ? text : '*Dile a la servidumbre que no me hable*'}`;
+    const htextos = `${text ? text : '*Chinguen a su madre*'}`;
     if ((isMedia && quoted.mtype === 'imageMessage') && htextos) {
       var mediax = await quoted.download?.();
       conn.sendMessage(m.chat, {image: mediax, mentions: users, caption: htextos, mentions: users}, {quoted: m});
@@ -36,11 +29,13 @@ const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
       var mediax = await quoted.download?.();
       conn.sendMessage(m.chat, {sticker: mediax, mentions: users}, {quoted: m});
     } else {
-      await conn.relayMessage(m.chat, {extendedTextMessage: {text: `${masss}\n${htextos}\n`, ...{contextInfo: {mentionedJid: users, externalAdReply: {thumbnail: imagen1, sourceUrl: 'https://chat.whatsapp.com/CCDGO1lIuH3Ggr4nA44cbY'}}}}}, {});
+      await conn.relayMessage(m.chat, {extendedTextMessage: {text: `${masss}\n${htextos}\n`, ...{contextInfo: {mentionedJid: users, externalAdReply: {thumbnail: imagen1, sourceUrl: 'instagram.com/xskycita.7 '}}}}}, {});
     }
   }
 };
-handler.command = /^(hidetag|notificar|notify)$/i;
+handler.help = ['hidetag']
+handler.tags = ['group']
+handler.command = /^(hidetag|notify|notificar|noti|n|hidetah|hidet)$/i;
 handler.group = true;
 handler.admin = true;
 export default handler;
